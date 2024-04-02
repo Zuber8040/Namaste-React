0f74095adc732cc49e7ecc80bd17612717f50163
Zuber Ustad <72431298+Zuber8040@users.noreply.github.com>
2024-04-02 22:21:25 +0530

    epside 01

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("h1", { id: "heading" }, "This is Zuber here i am learning  ");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement("div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        {id:"child"},
+        React.createElement("h1",{},"i am h1 tag"), ),
+);
+
+root.render(parent);
