88af2fcaaca75ad6e28a160c08d2112d58a8fb32
Zuber Ustad <72431298+Zuber8040@users.noreply.github.com>
2024-04-05 09:02:45 +0530

    epsidode 02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import  ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "heading" }, "This is Zuber here i am learning  ");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
